e6d7b45725032f5d82a12ce74d7fef84abac511f
Prapthi <prapthinalige@gmail.com>
2022-09-22 17:20:09 +0530

    Added new function

--- functions/exposurecalc/index.js
+++ functions/exposurecalc/index.js
@@ -1,5 +1,5 @@
 /**
- * Describe Exposurecalc here.
+ * Describe Myfunction here.
  *
  * The exported method is the entry point for your code when the function is invoked. 
  *
@@ -11,12 +11,50 @@
  *                 to a given execution of a function.
  */
  
-export default async function (event, context, logger) {
-    logger.info(`Invoking Exposurecalc with payload ${JSON.stringify(event.data || {})}`);
+ export default async function (event, context, logger) {
+    logger.info(
+        `Invoking salesforcesdkjs function with payload ${JSON.stringify(event.data || {})}`,
+    );
 
-    const results = await context.org.dataApi.query('SELECT Id, Name FROM Account');
+    // Extract properties from payload
+    const { loanAmount, borrowerType, borrower, totaldirect, totalindirect} = event.data;
 
-    logger.info(JSON.stringify(results));
+    if(borrowerType != 'Guarantor'){
+        const totalDirect = totaldirect+loanAmount;
 
-    return results;
+    }
+    else{
+        const totalInDirect = totalindirect+loanAmount;
+
+    }
+
+
+    const uow = context.org.dataApi.newUnitOfwork();
+
+    // Define a record using the RecordForCreate type and providing the Developer Name
+    const account = uow.registerUpdate(
+    {
+        type: "Account",
+        fields:{
+            Id: borrower,
+            TotalDirect: TotalDirect,
+            TotalInDirect:TotalInDirect
+        }
+    });
+
+
+    try {
+        // Insert the record using the SalesforceSDK DataApi and get the new Record Id from the result
+        const { id: recordId } = await context.org.dataApi.commitUnitOfWork(uow);
+
+        // Query Accounts using the SalesforceSDK DataApi to verify that your new Account was created.
+        const soql = `SELECT Fields(STANDARD) FROM Account`;// WHERE Id = '${recordId}'`;
+        const queryResults = await context.org.dataApi.query(soql);
+        return queryResults;
+    } catch (err) {
+        // Catch any DML errors and pass the throw an error with the message
+        const errorMessage = `Failed to update record. Root Cause: ${err.message}`;
+        logger.error(errorMessage);
+        throw new Error(errorMessage);
+    }
 }
